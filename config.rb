ac265c63607688018f69aa40128116aee445c0e6
pavlushka <pavlushka>
2014-11-06 20:31:24 +0300

    add first files

---
+++ config.rb
@@ -0,0 +1,8 @@
+# более подробная справка https://github.com/vaeum/config.rb
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "img"
+javascripts_dir = 'js'
+output_style = :expanded
+line_comments = false
+relative_assets = true
