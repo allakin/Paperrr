3c1ffc1018916b60d7cf3aaa1146b87e6f3e7f9b
pavlushka <pavlushka>
2014-11-08 19:44:47 +0300

    add slider

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.bxslider').bxSlider({
+        mode: 'fade',
+        nextSelector: '#slider-next',
+        prevSelector: '#slider-prev',
+        nextText: '',
+        pager: true,
+        prevText: ''
+    });
+});
